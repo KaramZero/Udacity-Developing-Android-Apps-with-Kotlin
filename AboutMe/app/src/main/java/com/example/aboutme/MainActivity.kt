797b04f067c4ae570d24049d4edaab2ea6d85149
Karam Ibrahim <karamzeroo@gmail.com>
2022-08-14 15:46:13 +0200

    Color My Views  app

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var myName:MyName = MyName("karam")
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root.rootView)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+        apply {
+            myName.nickname = binding.nicknameEdit.text.toString()
+            binding.invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
+        //hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
